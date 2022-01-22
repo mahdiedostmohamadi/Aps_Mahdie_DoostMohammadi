7c8738f552d3a4159d3ae9b5e92dd894010f8beb
mahdiedostmohamadi <92886168+mahdiedostmohamadi@users.noreply.github.com>
2022-01-22 15:39:03 +0330

    practicejs

---
+++ Folder_5/8-quiz2.js
@@ -0,0 +1,43 @@
+/*
+Task 1
+=======
+Write JavaScript below that logs:
+    1. all the "p" element nodes of the document,
+    --> should log a list of nodes with a length of 6
+    2. the first div element node
+    --> should log the ".site-header" node
+    3. the element with id "jumbotron-text"
+    --> should log the "#jumbotron-text" node
+    4. all the "p" elements of contained inside  the .primary-content element node
+    --> should log a list of nodes with a length of 3
+*/
+
+
+// const allPara  = document.getElementsByTagName('p')
+// console.log(allPara)
+
+// const firstDiv = document.querySelector('div')
+// console.log(firstDiv)
+
+// const jumbotron = document.getElementById('jumbotron-text')
+// console.log(jumbotron)
+
+// const allPInsideClass = document.querySelectorAll('.primary-content p')
+// console.log(allPInsideClass)
+
+
+
+
+
+
+/*
+Task 2
+======
+When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for vulnerable!"
+*/
+
+/*
+Task 3
+=======
+Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
+*/
